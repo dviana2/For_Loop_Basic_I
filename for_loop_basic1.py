ca386f9e7c1efdf42a251aa8d1833619c876f9d2
Dayry Viana <dayry.viana@gmail.com>
2021-11-02 19:42:59 -0400

    For Loop Assignment

---
+++ for_loop_basic1.py
@@ -0,0 +1,36 @@
+for i in range(0,151):
+    print(i)
+
+
+for i in range(5,1001):
+    if i % 5 == 0:
+        print(i)
+
+
+for i in range(0,101):
+    if i % 10 == 0:
+        print("Coding Dojo")
+    elif i % 5 == 0:
+        print("Coding")
+    else:
+        print (i)
+
+
+sum1 = 0
+for i in range(0,500001):
+    if i % 2 == 1:
+        sum1 += i
+print (sum1)
+
+
+for i in range (2018, -1, -4):
+    if i % 2 == 0:
+        print (i)
+
+lowNum= 2
+highNum= 10
+mult = 2
+
+for i in range (2,11):
+    if i % 2 == 0:
+        print (i)
